97b09966808037793fe00683a6a2dfc341b70d22
bushkovsky <62966446+bushkovsky@users.noreply.github.com>
2024-06-28 23:04:06 +0300

    add save method feature

--- Service/src/main/java/ru/platik777/service/Recomendation/Service.java
+++ Service/src/main/java/ru/platik777/service/Recomendation/Service.java
@@ -2,10 +2,20 @@ package ru.platik777.service.Recomendation;
 
 import org.FitBot.DtoGeneralCharect;
 import org.FitBot.DtoTrackInfo;
+import org.FitBot.ReadPort;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
+@org.springframework.stereotype.Service
 public class Service {
+
+    private ReadPort readPort;
+
+    @Autowired
+    public Service(ReadPort readPort) {
+        this.readPort = readPort;
+    }
 
     public DtoGeneralCharect getGeneralСharacteristics(List<DtoTrackInfo> dtoTrackInfos) {
         double genDistance = 0;
@@ -19,5 +29,10 @@ public class Service {
         }
 
         return new DtoGeneralCharect(genTime, genDistance, genHeight);
+    }
+
+    public void  addFitInfo(String pathToFitFile) {
+        readPort.read(pathToFitFile);
+        //взаимодействие с Data Access слоем
     }
 }
